7c1f8e54e178b1f769fe94488a732ce6f884a038
datoxx <davit.ardzenadze.05@gmail.com>
2023-01-18 18:30:59 +0400

    setup project

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app'
+import GlobalStyle from '../components/globalstyles'
+
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+        <GlobalStyle />
+        <Component {...pageProps} />
+    </>
+  )
+}
